f27fc038cb02d05334cf3f5e7d59f399628184ca
Chii Yuen <chii.yuen@hotmail.com>
2025-10-23 18:23:01 +0800

    feat: refactor Jupiter API helpers

---
+++ lib/api/jupiter.ts
@@ -0,0 +1,49 @@
+export async function getPrices(mint: string[]) {
+  const url = new URL(`${process.env.JUPITER_API_URL}/price/v3`);
+
+  url.searchParams.append('ids', mint.join(','));
+
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.error || 'Unable to fetch token prices from Jupiter API.');
+  }
+
+  const prices: Record<string, number> = {};
+
+  for (const key in data) {
+    prices[key] = data[key].usdPrice;
+  }
+
+  return prices;
+}
+
+export async function getTokenInfos(mint: string[]) {
+  const url = new URL(`${process.env.JUPITER_API_URL}/tokens/v2/search`);
+
+  url.searchParams.append('query', mint.join(','));
+
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.error || 'Unable to fetch token info from Jupiter API.');
+  }
+
+  const infos: Record<string, any> = {};
+
+  data.forEach((tokenInfo: any) => {
+    infos[tokenInfo.id] = {
+      name: tokenInfo.name,
+      symbol: tokenInfo.symbol,
+      icon: tokenInfo.icon,
+      decimals: tokenInfo.decimals,
+      tokenProgram: tokenInfo.tokenProgram
+    }
+  })
+
+  return infos;
+}
