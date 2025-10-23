0570a8907cb1551e0a08846b5cbfba65fd50a830
Chii Yuen <chii.yuen@hotmail.com>
2025-10-23 17:58:49 +0800

    feat: refactor transaction fetching from backend

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -26,3 +26,22 @@ export function stringToByteArray(str: string, length: number): number[] {
 export function v0TxToBase64(tx: VersionedTransaction): string {
   return Buffer.from(tx.serialize()).toString("base64");
 }
+
+export function base64ToV0Tx(base64: string): VersionedTransaction {
+  const buffer = Buffer.from(base64, "base64");
+  return VersionedTransaction.deserialize(Uint8Array.from(buffer));
+}
+
+export async function wrappedFetch(url: string, method: string = 'GET', body: any = null) {
+  const res = await fetch(url, {
+    method,
+    body,
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.error);
+  }
+
+  return data;
+}
