2b8636bf7eb44f5138fd5bc1b35a3632c755d250
Chii Yuen <chii.yuen@hotmail.com>
2025-10-24 20:20:03 +0800

    chore: simplify getVaultStrategies and getAllVaults
    wrappedFetch already handles response parsing and error handling

--- lib/api/accounts.ts
+++ lib/api/accounts.ts
@@ -1,23 +1,10 @@
 import { PublicKey } from "@solana/web3.js";
+import { wrappedFetch } from "../utils";
 
 export async function getVaultStrategies(vault: PublicKey) {
-    try {
-        const res = await fetch(`/api/accounts/strategies?vault=${vault.toString()}`);
-        const data = await res.json();
-        return data.strategies;
-    } catch (error) {
-        console.error("Fetch Vault Strategies Error:", error);
-        return null;
-    }
+    return await wrappedFetch(`/api/accounts/strategies?vault=${vault.toString()}`);
 }
 
 export async function getAllVaults() {
-    try {
-        const res = await fetch(`/api/accounts/vaults`);
-        const data = await res.json();
-        return data.vaults;
-    } catch (error) {
-        console.error("Fetch All Vaults Error:", error);
-        return null;
-    }
+    return await wrappedFetch(`/api/accounts/vaults`);
 }
