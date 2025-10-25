a73e0c91ee4b7af31e2fdfa2b387edaebef57e1c
Noobmaster169 <mariotaning@gmail.com>
2025-10-25 22:26:45 +0800

    fix: API error in vault initialization

--- lib/utils/mirrorfi-client.ts
+++ lib/utils/mirrorfi-client.ts
@@ -23,7 +23,7 @@ export class MirrorFiClient {
     )[0];
   }
 
-  private getTreasuryPda() {
+  getTreasuryPda() {
     return PublicKey.findProgramAddressSync(
       [Buffer.from("treasury")],
       programId,
